20b40fd288ba86f7053d48734ba08a4d4b880912
Celina <marisa.celina.lopez4@gmail.com>
2022-07-29 18:19:52 -0700

    make basic account page

--- apollo-client.js
+++ apollo-client.js
@@ -1,4 +1,6 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
 
 const defaultOptions = {
     watchQuery: {
@@ -10,6 +12,26 @@ const defaultOptions = {
         errorPolicy: 'all',
     },
 }
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:3000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = window.localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+export const authenticatedClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions
+});
 
 const client = new ApolloClient({
     uri: "http://localhost:3000/graphql",
